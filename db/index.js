c35ada2a95ce27c1c2cb23036cb746424200a3c3
Andrew <oleandy1221@gmail.com>
2021-03-08 17:54:40 -0600

    tables

--- db/index.js
+++ db/index.js
@@ -1,11 +1,14 @@
 const { Client } = require("pg");
 
-const DB_NAME = "graceshopper";
+const DB_NAME = "furrbanking";
 const DB_URL = process.env.DATABASE_URL || `postgres://localhost/${DB_NAME}`;
 const client = new Client({
   connectionString: DB_URL,
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : undefined,
 });
+
+const bcrypt = require("bcrypt");
+const { response, query } = require("express");
 
 module.exports = {
     client
--- db/init.js
+++ db/init.js
@@ -1,1 +1,41 @@
 const { client } = require("./index");
+
+async function buildTables() {
+    try {
+        client.connect();
+
+        console.log('Start Dropping Tables');
+        await client.query(`
+            DROP TABLE IF EXISTS
+        `)
+        console.log('finished dropping tables');
+
+        console.log('started building tables');
+        await client.query(`
+            DO $$
+                BEGIN
+                    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'stats') THEN
+                    CREATE TYPE stats AS ENUM ('created', 'cancelled', 'completed', 'processing');
+                END IF;
+            END
+            $$;
+            
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                "firstName" VARCHAR(255) NOT NULL,
+                "lastName" VARCHAR(255) NOT NULL,
+                username VARCHAR(255) NOT NULL
+            )
+        `)
+
+
+
+    } catch(error) {
+        throw error;
+    }
+}
+
+buildTables()
+  .then(populateInitialData)
+  .catch(console.error)
+  .finally(() => client.end());
